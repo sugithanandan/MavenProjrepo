6ff940f9130b2da71ea2d61277b744b7177802e6
sugramal <sugramal@SUGRAMAL-EGE3J.partnet.cisco.com>
2017-04-12 12:35:55 +0530

    test commit

---
+++ testmaven/src/main/java/com/cisco/cpnr/diana/testmaven/App.java
@@ -0,0 +1,14 @@
+package com.cisco.cpnr.diana.testmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void firsttest()
+    {
+    	System.out.println("Hi welcome to maven");
+    }
+    
+}
---
+++ testmaven/src/test/java/com/cisco/cpnr/diana/testmaven/AppTest.java
@@ -0,0 +1,18 @@
+package com.cisco.cpnr.diana.testmaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    @Test
+    public void test()
+    {
+    	App a = new App();
+    	a.firsttest();
+    	
+    }
+}
