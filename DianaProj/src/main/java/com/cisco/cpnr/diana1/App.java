d2aa1e201d7e0fcca2b5c7942c9973a89667c7d1
sugramal <sugramal@SUGRAMAL-EGE3J.partnet.cisco.com>
2017-04-12 12:17:42 +0530

    maven proj

---
+++ DianaProj/src/main/java/com/cisco/cpnr/diana1/App.java
@@ -0,0 +1,13 @@
+package com.cisco.cpnr.diana1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void testMethod()
+    {
+    	System.out.println("Hi I am Maven Project");
+    }
+}
